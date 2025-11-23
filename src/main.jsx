40c293a5871202c6fa117dd75c094e5e760dd3ec
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-11-23 19:06:36 +0600

    initialize react ap pwith vite, tailwind, daisyui, routing setup, and base ui pages

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { ToastContainer } from "react-toastify";
+import { router } from "./routes";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+    <ToastContainer />
+  </StrictMode>
+);
---
+++ src/routes.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "./layout/MainLayout";
+import Homepage from "./pages/Homepage";
+import AboutUs from "./pages/AboutUs";
+import Profile from "./pages/Profile";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Homepage /> },
+      { path: "/about-us", element: <AboutUs /> },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
