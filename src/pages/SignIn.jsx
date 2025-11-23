2dfcfa5646f38d90496bc557fd0450bc85070ef7
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-11-24 04:47:47 +0600

    add password visibility toggle, google authentication, and user session ui to signin component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,71 @@
 import { Link } from "react-router";
 import MyContainer from "../components/MyContainer";
+import { useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { IoEyeOff } from "react-icons/io5";
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { toast } from "react-toastify";
+import { auth } from "../firebase/firebase.config";
+
+const googleProvider = new GoogleAuthProvider();
 
 const SignIn = () => {
-  const handleSignin = () => {};
+  const [user, setUser] = useState(null);
+  const [show, setShow] = useState(false);
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    console.log("signin function entered!", { email, password });
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setUser(user);
+        toast.success("Logged in successfully!");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message);
+      });
+  };
   const handleForgetPassword = () => {};
-  const handleGoogleSignin = () => {};
+  const handleGoogleSignin = () => {
+    console.log("google sign in");
+    
+    signInWithPopup(auth, googleProvider)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setUser(user);
+        toast.success("Logged in successfully!");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message);
+      });
+  };
   const handleGithubSignin = () => {};
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success("Signout successful");
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="min-h-[calc(100vh-20px)] flex items-center justify-center bg-linear-to-br from-blue-500 via-indigo-600 to-purple-600 relative overflow-hidden">
       {/* Animated glow orbs */}
@@ -29,95 +89,114 @@ const SignIn = () => {
 
           {/* Login card */}
           <div className="w-full max-w-md backdrop-blur-lg bg-white/10 border border-white/20 shadow-2xl rounded-2xl p-8">
-            <form onSubmit={handleSignin} className="space-y-5">
-              <h2 className="text-2xl font-semibold mb-2 text-center text-white">
-                Sign In
-              </h2>
-
-              <div>
-                <label className="block text-sm mb-1">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  // value={email}
-                  // onChange={(e) => setEmail(e.target.value)}
-                  placeholder="example@email.com"
-                  className="input input-bordered w-full bg-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                />
-              </div>
-
-              <div className="relative">
-                <label className="block text-sm mb-1">Password</label>
-                <input
-                  type="text"
-                  name="password"
-                  placeholder="••••••••"
-                  className="input input-bordered w-full bg-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                />
-                <span
-                  onClick={() => setShow(!show)}
-                  className="absolute right-2 top-9 cursor-pointer z-50"
-                >
-                </span>
-              </div>
-
-              <button
-                className="hover:underline cursor-pointer"
-                onClick={handleForgetPassword}
-                type="button"
-              >
-                Forget password?
-              </button>
-
-              <button type="submit" className="my-btn">
-                Login
-              </button>
-
-              {/* Divider */}
-              <div className="flex items-center justify-center gap-2 my-2">
-                <div className="h-px w-16 bg-white/30"></div>
-                <span className="text-sm text-white/70">or</span>
-                <div className="h-px w-16 bg-white/30"></div>
-              </div>
-
-              {/* Google Signin */}
-              <button
-                type="button"
-                onClick={handleGoogleSignin}
-                className="flex items-center justify-center gap-3 bg-white text-gray-800 px-5 py-2 rounded-lg w-full font-semibold hover:bg-gray-100 transition-colors cursor-pointer"
-              >
+            {user ? (
+              <div className="text-center space-y-3">
                 <img
-                  src="https://www.svgrepo.com/show/475656/google-color.svg"
-                  alt="google"
-                  className="w-5 h-5"
+                  src={user?.photoURL || "https//via.placeholder.com/88"}
+                  className="h-20 w-20 rounded-full mx-auto"
+                  alt=""
                 />
-                Continue with Google
-              </button>
+                <h2 className="text-xl font-semibold"> {user?.displayName} </h2>
+                <p className="text-lg text-white/80"> {user?.email} </p>
+                <button onClick={handleSignOut} className="my-btn">
+                  Sign Out
+                </button>
+              </div>
+            ) : (
+              <form onSubmit={handleSignin} className="space-y-5">
+                <h2 className="text-2xl font-semibold mb-2 text-center text-white">
+                  Sign In
+                </h2>
 
-              {/* Github Signin */}
-              <button
-                type="button"
-                onClick={handleGithubSignin}
-                className="flex items-center justify-center gap-3 bg-white text-gray-800 px-5 py-2 rounded-lg w-full font-semibold hover:bg-gray-100 transition-colors cursor-pointer"
-              >
-                <img
-                  src="https://img.icons8.com/fluency/48/github.png"
-                  alt="google"
-                  className="w-5 h-5"
-                />
-                Continue with Github
-              </button>
+                <div>
+                  <label className="block text-sm mb-1">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    // value={email}
+                    // onChange={(e) => setEmail(e.target.value)}
+                    placeholder="example@email.com"
+                    className="input input-bordered w-full bg-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  />
+                </div>
 
-              <p className="text-center text-sm text-white/80 mt-3">
-                Don’t have an account?{" "}
-                <Link
-                  to="/signup"
-                  className="text-pink-300 hover:text-white underline"
+                <div className="relative">
+                  <label className="block text-sm mb-1" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    type={show ? "text" : "password"}
+                    name="password"
+                    placeholder="••••••••"
+                    className="input input-bordered w-full bg-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                  />
+                  <span
+                    onClick={() => setShow(!show)}
+                    className="absolute right-2 top-9 cursor-pointer z-50"
+                  >
+                    {show ? <FaEye /> : <IoEyeOff />}
+                  </span>
+                </div>
+
+                <button
+                  className="hover:underline cursor-pointer"
+                  onClick={handleForgetPassword}
+                  type="button"
                 >
-                  Sign up
-                </Link>
-              </p>
-            </form>
+                  Forget password?
+                </button>
+
+                <button type="submit" className="my-btn">
+                  Login
+                </button>
+
+                {/* Divider */}
+                <div className="flex items-center justify-center gap-2 my-2">
+                  <div className="h-px w-16 bg-white/30"></div>
+                  <span className="text-sm text-white/70">or</span>
+                  <div className="h-px w-16 bg-white/30"></div>
+                </div>
+
+                {/* Google Signin */}
+                <button
+                  type="button"
+                  onClick={handleGoogleSignin}
+                  className="flex items-center justify-center gap-3 bg-white text-gray-800 px-5 py-2 rounded-lg w-full font-semibold hover:bg-gray-100 transition-colors cursor-pointer"
+                >
+                  <img
+                    src="https://www.svgrepo.com/show/475656/google-color.svg"
+                    alt="google"
+                    className="w-5 h-5"
+                  />
+                  Continue with Google
+                </button>
+
+                {/* Github Signin */}
+                <button
+                  type="button"
+                  onClick={handleGithubSignin}
+                  className="flex items-center justify-center gap-3 bg-white text-gray-800 px-5 py-2 rounded-lg w-full font-semibold hover:bg-gray-100 transition-colors cursor-pointer"
+                >
+                  <img
+                    src="https://img.icons8.com/fluency/48/github.png"
+                    alt="google"
+                    className="w-5 h-5"
+                  />
+                  Continue with Github
+                </button>
+
+                <p className="text-center text-sm text-white/80 mt-3">
+                  Don’t have an account?{" "}
+                  <Link
+                    to="/signup"
+                    className="text-pink-300 hover:text-white underline"
+                  >
+                    Sign up
+                  </Link>
+                </p>
+              </form>
+            )}
           </div>
         </div>
       </MyContainer>
